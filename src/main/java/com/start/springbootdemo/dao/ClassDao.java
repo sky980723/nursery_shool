f3b91f9457113df8e2709eecf090c8f0b565d8c3
sky980723 <917968474@qq.com>
2020-08-17 22:50:00 +0800

    班级的添加接口

--- src/main/java/com/start/springbootdemo/dao/ClassDao.java
+++ src/main/java/com/start/springbootdemo/dao/ClassDao.java
@@ -1,4 +1,9 @@
 package com.start.springbootdemo.dao;
 
+import com.start.springbootdemo.entity.Class;
+
 public interface ClassDao {
+    Integer saveClass(Class studentClass);
+
+    Integer updateClass(Class studentClass);
 }
--- src/main/java/com/start/springbootdemo/service/impl/ClassServiceImpl.java
+++ src/main/java/com/start/springbootdemo/service/impl/ClassServiceImpl.java
@@ -3,9 +3,13 @@ package com.start.springbootdemo.service.impl;
 import com.start.springbootdemo.dao.ClassDao;
 import com.start.springbootdemo.entity.Class;
 import com.start.springbootdemo.service.IClassService;
+import com.start.springbootdemo.util.KeyGen;
 import com.start.springbootdemo.util.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
 
 @Service
 public class ClassServiceImpl implements IClassService {
@@ -18,6 +22,18 @@ public class ClassServiceImpl implements IClassService {
     public Results<String> saveOrUpdateClass(Class studentClass) {
         Results<String> results = new Results<>();
         //根据id是否存在判断是添加还是修改
-        return null;
+        if (StringUtils.isEmpty(studentClass.getId())) {
+            //为空，需要添加班级
+            studentClass.setId(KeyGen.uuid());
+            studentClass.setAddtime(new Date());
+            classDao.saveClass(studentClass);
+        }else {
+            //不为空，走修改
+            studentClass.setUpdatetime(new Date());
+            classDao.updateClass(studentClass);
+        }
+        results.setStatus("0");
+
+        return results;
     }
 }
