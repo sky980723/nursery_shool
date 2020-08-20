635f3de511d56aa59b1984c1834bc8a328586ae1
sky980723 <917968474@qq.com>
2020-08-20 17:40:14 +0800

    增加引入的jar包/增加注释

--- src/main/java/com/start/springbootdemo/controller/BannerController.java
+++ src/main/java/com/start/springbootdemo/controller/BannerController.java
@@ -17,10 +17,17 @@ public class BannerController {
     @Autowired
     private IBannerService bannerService;
 
+    /**
+     * 根据学校ID查询banner的集合，按添加顺序倒序展示
+     *
+     * @param schoolId 学校ID
+     * @param types    banner的类型，目前只有一类，但是后续可能会增加其他类型
+     * @return
+     */
     @GetMapping(value = "/listBanner")
-    public Results<List<Banner>> listBanner(@RequestParam(name = "schoolId")String schoolId,
-                                            @RequestParam(name = "types",required = false)Integer types) {
+    public Results<List<Banner>> listBanner(@RequestParam(name = "schoolId") String schoolId,
+                                            @RequestParam(name = "types", required = false) Integer types) {
 
-        return bannerService.listBanner(schoolId,types);
+        return bannerService.listBanner(schoolId, types);
     }
 }
