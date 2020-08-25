a3271bfe84134704c13ea03dee252e5458546015
sky980723 <917968474@qq.com>
2020-08-25 14:38:03 +0800

    filter 过滤器支持跨域/主启动类增加开启事务的注解

--- src/main/java/com/start/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/start/springbootdemo/SpringbootDemoApplication.java
@@ -3,11 +3,17 @@ package com.start.springbootdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Sky
  */
 @SpringBootApplication
+@EnableScheduling
+@EnableSwagger2
+@EnableTransactionManagement
 @MapperScan("com.start.springbootdemo.dao")
 public class SpringbootDemoApplication {
 
---
+++ src/main/java/com/start/springbootdemo/config/GlobalCorsConfig.java
@@ -0,0 +1,40 @@
+package com.start.springbootdemo.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * filter 过滤器,支持全局跨域请求
+ * @author Administrator
+ */
+@Component
+public class GlobalCorsConfig implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("Access-Control-Allow-Credentials", "true");
+        res.addHeader("Access-Control-Allow-Origin", "*");
+        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN");
+        if ("OPTIONS".equals(((HttpServletRequest) request).getMethod())) {
+            response.getWriter().println("ok");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+}
