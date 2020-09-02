8eb1ae680b839dcccf10e185a66b3a96833ef04d
sky980723 <917968474@qq.com>
2020-09-02 17:45:48 +0800

    修复查询年级时不返回年级name的bug

--- src/main/java/com/start/springbootdemo/service/impl/IndexServiceImpl.java
+++ src/main/java/com/start/springbootdemo/service/impl/IndexServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
-import sun.plugin2.message.SetAppletSizeMessage;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
